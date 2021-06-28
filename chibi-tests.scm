31fb9a5db9fea24aa0a702af4319ee4c0dec1f7c
John Cowan <jcowan@ebrevia.com>
2021-06-28 14:04:57 -0400

    Initial state: TimeObject.md + SRFIs 19 and 174

---
+++ chibi-tests.scm
@@ -0,0 +1,31 @@
+(import (scheme base))
+(import (chibi test))
+(import (srfi 174))
+
+(define ts1 (timespec 1 2))
+(define ts2 (timespec 1 2))
+(define ts3 (timespec 1 3))
+(define ts4 (timespec 2 2))
+(define ts-neg1 (timespec -1 2))
+(define ts-neg2 (timespec -1 5))
+(define ts-neg3 (timespec -2 0))
+
+(test-group "srfi-174"
+
+(test-assert "timespec?" (timespec? ts1))
+(test-assert "not timespec?" (not (timespec? #f)))
+(test "seconds" 1 (timespec-seconds ts1))
+(test "nanos" 2 (timespec-nanoseconds ts1))
+(test-assert "equal" (timespec=? ts1 ts2))
+(test-assert "less nanos" (timespec<? ts1 ts3))
+(test-assert "less seconds" (timespec<? ts1 ts4))
+(test-assert "less -nanos" (timespec<? ts-neg2 ts-neg1))
+(test-assert "less -seconds" (timespec<? ts-neg3 ts-neg2))
+(test-assert "positive hash" (positive? (timespec-hash ts-neg1)))
+(test "to inexact" #i1.1 (timespec->inexact (timespec 1 #e1e8)))
+(let ((t (timespec 1 1))
+      (u (inexact->timespec #i1.000000001)))
+  (test "from inexact" t u))
+)
+
+(test-exit)
